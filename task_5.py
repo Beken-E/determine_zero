9b480c75400aa8c4dc8b4ef0293cfd9b17dee683
Beken-E <besnmnv@gmail.com>
2020-05-04 19:32:25 +0600

    File with code

---
+++ task_5.py
@@ -0,0 +1,11 @@
+x = input("Give your number: ")
+
+x = int(x)
+
+if x < 0:
+    print('your number is negative')
+elif x > 0:
+    print('your number is positive')
+else:
+    print("your number is zero") 
+       
